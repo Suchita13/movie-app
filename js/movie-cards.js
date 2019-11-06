33c3fe40445326d7d20d657ca979a308d24f6716
Suchita13 <53253850+Suchita13@users.noreply.github.com>
2019-11-06 10:51:38 +0530

    Movie Detail Module

--- js/movie-cards.js
+++ js/movie-cards.js
@@ -1,49 +1,62 @@
-var movies = (function(movies){
+import { genresNames } from './genres-name.js';
+import { quickView } from './quick-view.js';
 
-/*movie cards function*/
-movies.prepareCard = (movies) => {
-	console.log(movies);
-	//debugger;
-	var average = Math.floor(movies.vote_average/2);
-	var i=1;
-	let cardTemplate = `<section class='movielist'>
-							<figure>
-								<img src='https://image.tmdb.org/t/p/w500/${movies.poster_path}' class='movie-img'>
-							</figure>
-							<span class='left-span'>
-								<strong>${movies.title}</strong>
-							</span>
-							<span class='right-span fa fa-heart-o fa-lg' style='display:none;'>
-							</span>
-							<span class='right-span fa fa-heart fa-lg' style='display:none;'>
-							</span>
-							<p class='clearfix'>${movies.genre_ids}</p>`;
-						while(i <= 5){
-							if(i<=average){
-		cardTemplate =  cardTemplate + `<i class='fa fa-star star'></i>`;
-							}else{
-		cardTemplate =  cardTemplate + `<i class='fa fa-star-o star'></i>`;
-							}
-						i++;	
-						}	
-		cardTemplate =  cardTemplate+`
-							<a href='#'>Show more</a>
-		</section>`;	
+const API_KEY ='93dc8f056bd26a4e357c936b45bd9e37';
 
-  return cardTemplate;
+const genresNamesApi = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`;
+
+const getmovieDetail = (id) =>{
+	const api = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US&append_to_response=credits`;
+	fetch(api)
+	.then((movies) => {
+		return movies.json();
+	}).then((data) => {
+		quickView(data);
+	});	
+}
+
+export const prepareCard = (movies,idx) => {
+	let i=0;
+	movies.results.slice(0,4).map((card) => {
+		//console.log(card);
+		let k=1;
+		const average = Math.floor(card.vote_average/2);
+	  	const node = document.getElementsByTagName('template')[0];
+	  	const container = document.querySelectorAll(".movie-section")[idx];
+	  	const section = document.importNode(node.content, true);
+	  	let title = section.querySelectorAll(".left-span")[0];
+	  		title.textContent = card.title;
+	  	let img_src = section.querySelectorAll(".movie-img")[0];
+	  		img_src.setAttribute('src','https://image.tmdb.org/t/p/w500/' + card.poster_path);
+	  	let genres = section.querySelectorAll('.genres')[0];
+	  	let genre_name = genresNames(card.genre_ids, genresNamesApi);
+	  		genres.textContent = genre_name;
+	  	let hearts = section.querySelectorAll(".hearts")[0];
+	  	if(i%2==0){
+	  		hearts.innerHTML+= `<i class='fa fa-heart fa-lg'></i>`;
+	  	}else{
+	  		hearts.innerHTML+= `<i class='fa fa-heart-o fa-lg'></i>`;
+	  	}
+	  	let ratings = section.querySelectorAll(".ratings")[0];
+	  	while(k<=5) {
+			if(k<=average){
+				ratings.innerHTML+= `<i class='fa fa-star star'></i>`;
+			}else{
+				ratings.innerHTML+= `<i class='fa fa-star-o star'></i>`;
+			}
+			k++;
+	  	}
+	  	const link = section.querySelectorAll(".show-more")[0];
+	  		link.setAttribute('id',`show_${card.id}`);
+	  		link.addEventListener('click',() => {window.open(`show-details.html?movie=${card.id}`,'_self');},false);
+	  		img_src.setAttribute('id',card.id);
+	  		img_src.addEventListener('click',() => {getmovieDetail(card.id) },false);
+
+  	 	container.appendChild(section);
+    	i++;
+  	});
 }	
 
-/*movie generes id function*/
-
-/*movies.getMovieGeneres = () =>{
-	    console.log('abc');
-		const url ='https://api.themoviedb.org/3/genre/movie/list?api_key=93dc8f056bd26a4e357c936b45bd9e37&language=en-US';
-	    fetch(url).then((res)=>res.json()).then(function(result){
-         console.log(genre_ids);
-       });
-}*/
-
-return movies;
 
 
-})(movies || {});
+
